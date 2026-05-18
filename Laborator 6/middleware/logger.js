2f7a700ac11f0e2a630ba868ff662f1280592fe8
Alex <vintalexandru03@gmail.com>
2026-05-18 15:30:03 +0300

    feat: laborator 6: views

--- Laborator 6/middleware/logger.js
+++ Laborator 6/middleware/logger.js
@@ -1,4 +1,3 @@
-// Middleware de logging: noteaza in consola fiecare cerere si utilizatorul.
 module.exports = function logger(req, res, next) {
     const user = req.session && req.session.utilizator
         ? req.session.utilizator.email
--- Laborator 6/middleware/requireLogin.js
+++ Laborator 6/middleware/requireLogin.js
@@ -1,4 +1,3 @@
-// Middleware de protectie: lasa cererea sa treaca doar daca exista o sesiune logata.
 module.exports = function requireLogin(req, res, next) {
     if (req.session && req.session.utilizator) {
         return next();
