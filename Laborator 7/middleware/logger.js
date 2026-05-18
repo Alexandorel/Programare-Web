8a50303e2a3d2cbfced1c097c90ba0d4b0b1d788
Alex <vintalexandru03@gmail.com>
2026-05-18 12:50:04 +0300

    feat: laborator 7: middleware/logger.js

---
+++ Laborator 7/middleware/logger.js
@@ -0,0 +1,7 @@
+module.exports = function logger(req, res, next) {
+    const user = req.session && req.session.utilizator
+        ? req.session.utilizator.email
+        : 'anonim';
+    console.log(`${req.method} ${req.url} - user: ${user}`);
+    next();
+};
