9fea91e037b024719b701899f056fe474299b831
Alex <vintalexandru03@gmail.com>
2026-06-13 11:28:48 +0300

    bcrypt library

--- proiect/db/users.js
+++ proiect/db/users.js
@@ -1,7 +1,8 @@
 const { mongoose } = require('./index');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true, trim: true }
+    username: { type: String, required: true, unique: true, trim: true },
+    passwordHash: { type: String, required: true } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
